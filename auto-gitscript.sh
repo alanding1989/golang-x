23b93475e51c1b094c06581a0b07131ed3cc7b13
alanding <dw395577197@163.com>
2019-08-23 01:42:41 +0800

    update

---
+++ auto-gitscript.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+
+# File Name    : che.sh
+# Author       : AlanDing
+# Created Time : Fri 23 Aug 2019 01:33:07 AM CST
+# Description  : 
+
+
+cpath=$(pwd)
+
+#ls | while read file
+
+for file in $(ls) ; do
+  echo "$file"
+  if [ -d "$file" ]; then
+    cd "$file" || exit 1
+    git checkout master
+    # git pull origin master:master
+    cd "$cpath" || exit 1
+  fi
+done
