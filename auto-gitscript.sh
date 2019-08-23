0547e0521b4fbae76cfdd9d18a7661ccc373a2bd
alanding <dw395577197@163.com>
2019-08-23 12:26:36 +0800

    update

--- auto-gitscript.sh
+++ auto-gitscript.sh
@@ -7,18 +7,6 @@
 # Description  : auto pull all dirs
 
 
-cpath=$(pwd)
-
-#ls | while read file
 
 
 
-for file in $(ls) ; do
-  echo "$file"
-  if [ -d "$file" ]; then
-    cd "$file" || exit 1
-    git checkout master
-    git pull origin master:master
-    cd "$cpath" || exit 1
-  fi
-done
